480b1fe8302e6224ac24838e47e867adfa0c0342
rai <Minezers@gmail.com>
2022-08-07 15:08:19 -0400

    log messages to console

--- src/main/java/cool/coolplugin/listeners/ServerListener.java
+++ src/main/java/cool/coolplugin/listeners/ServerListener.java
@@ -1,5 +1,6 @@
 package cool.coolplugin.listeners;
 
+import cool.coolplugin.CoolPlugin;
 import cool.coolplugin.player.CoolPluginPlayer;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -8,36 +9,46 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.logging.Level;
+
 import static cool.coolplugin.CoolPlugin.*;
 
 public class ServerListener implements Listener
 {
+    private static CoolPlugin plugin;
+
+    public ServerListener(CoolPlugin plugin)
+    {
+        ServerListener.plugin = plugin;
+    }
+
     @EventHandler
     public void onMessageSend(AsyncPlayerChatEvent event)
     {
         Player player = event.getPlayer();
-        String message;
-        String format;
+        String message = null;
+        String format = null;
 
         for (Player recipient : event.getRecipients())
         {
             message = event.getMessage();
 
             // username: text
-            format =
-                    CoolPluginPlayer.getPrefix(player) +
-                    CoolPluginPlayer.getNameStylized(player) + "&7: &r";
+            format = CoolPluginPlayer.getPrefix(player) +
+                     CoolPluginPlayer.getNameStylized(player) + "&7: &r";
 
             // Ping players if mentioned
             if (message.contains(recipient.getName()))
             {
-                message = message.replace(recipient.getName(), "&a" + recipient.getName() + "&r");
+                message = message.replace(recipient.getName(), "&a&o" + recipient.getName() + "&r");
                 recipient.playSound(recipient.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 2.0f);
             }
 
-            format += message;
+            format = format.concat(message);
             recipient.sendMessage(colorize(format));
         }
+        // Send to console since event gets canceled
+        plugin.getLogger().log(Level.INFO, player.getName() + "(" + CoolPluginPlayer.getNickname(player) + ") : " + message);
 
         event.setCancelled(true);
     }
@@ -47,13 +58,12 @@ public class ServerListener implements Listener
     {
         Player player = event.getPlayer();
 
+        // Check if user is not in data.yml
         // Apply changes to data.yml and save
         if (!CoolPluginPlayer.hasNickname(player))
         {
-            data.getConfig().set("players." + player.getName().toLowerCase() + ".uuid", player.getUniqueId().toString());
-            data.getConfig().set("players." + player.getName().toLowerCase() + ".nickname", player.getName());
+            CoolPluginPlayer.grabUUID(player);
+            CoolPluginPlayer.setNickname(player, player.getName());
         }
-
-        data.saveConfig();
     }
 }
